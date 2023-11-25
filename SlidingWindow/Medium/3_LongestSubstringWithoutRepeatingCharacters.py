08afdee8be947324ee050b2ab7caa8aa08aa4721
Nicholas Tanzillo <95975957+NicholasTanz@users.noreply.github.com>
2023-11-24 21:32:00 -0500

    Update 3_LongestSubstringWithoutRepeatingCharacters.py

--- SlidingWindow/Medium/3_LongestSubstringWithoutRepeatingCharacters.py
+++ SlidingWindow/Medium/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,36 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        present_chars = {}
-        l_idx, r_idx = 0, 0
-        max_substr = 0
-        curr_len = 0
-        l_s = len(s)
-        if(not l_s):
-            return 0
-        while r_idx != l_s and l_s - l_idx > max_substr:
-            if(s[r_idx] in present_chars):
-                if(curr_len > max_substr):
-                    max_substr = curr_len
-                present_chars = {}
-                curr_len=0
-                l_idx+=1
-                r_idx=l_idx
-            else:
-                present_chars[s[r_idx]] = True
-                curr_len+=1
-                r_idx+=1
-        if(curr_len > max_substr):
-            return curr_len
-        return max_substr
-# Optimized Solution:
-'''
-charSet = set()
-l, res = 0, 0
-for r in range(len(s)):
-    while s[r] in charSet:
-        charSet.remove(s[l])
-        l += 1
-    charSet.add(s[r]
-    res = max(res, r - 1 + 1)
-return res
-'''
+        charSet = set()
+        l, r = 0, 0
+        maxSub = 0
+
+        while r < len(s):
+            while s[r] in charSet:
+               charSet.remove(s[l])
+               l += 1
+            charSet.add(s[r])
+            maxSub = max(r - l + 1, maxSub)
+            r += 1        
+
+        return maxSub
