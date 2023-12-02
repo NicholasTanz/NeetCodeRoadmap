599c3ee37b1d6867f4ef3fc27ed8bf1fb4b28e42
Nicholas Tanzillo <95975957+NicholasTanz@users.noreply.github.com>
2023-12-02 13:13:28 -0500

    Create 53_JumpGame.py

---
+++ Greedy/Medium/53_JumpGame.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        end = len(nums) - 1 
+
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] + i >= end:
+                end = i
+
+        return True if end == 0 else False
