e8ac97ac888fb93e31d941a917b5a2f47b3dc9d6
Nicholas Tanzillo <95975957+NicholasTanz@users.noreply.github.com>
2023-12-02 00:01:50 -0500

    Update 567_PermutationInString.py

--- SlidingWindow/Medium/567_PermutationInString.py
+++ SlidingWindow/Medium/567_PermutationInString.py
@@ -1,42 +1,35 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_len = len(s1)
-        s2_len = len(s2)
-        if(s2_len < s1_len):
+        if(len(s1) > len(s2)):
             return False
         
-        # assign all chars in s1 to dict
-        chars_in_s1 = {}
-        for char in s1:
-            if char in chars_in_s1:
-                chars_in_s1[char]+=1
-            else:
-                chars_in_s1[char]=1
+        s1Count, s2Count = [0]*26, [0]*26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1Count[i] == s2Count[i] else 0
         
-        l_idx, r_idx = 0, 0
+        l = 0
+        for r in range(len(s1), len(s2)): # start at nxt char that left off
+            if(matches == 26):
+                return True
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
 
-        copy_chars_in_s1 = {key:value for key, value in chars_in_s1.items()}
-        while(r_idx != s2_len and s2_len - l_idx >= s1_len):
-           # if char in s2, is in dict reprsenting all chars in s1
-            if(s2[r_idx] in copy_chars_in_s1):
-               # if char hasn't been all used up
-               if(copy_chars_in_s1[s2[r_idx]] > 0):
-                    copy_chars_in_s1[s2[r_idx]]-=1
-                    r_idx+=1
-               else:
-                    copy_chars_in_s1 = {key:value for key, value in chars_in_s1.items()}
-                    l_idx+=1
-                    r_idx=l_idx
-               if(r_idx - l_idx == s1_len):
-                    return True
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1 
+
+        return matches == 26
             
-            #char in s2 is not present in dict reprsenting all chars in s1
-            else:
-                if(l_idx != r_idx):
-                    copy_chars_in_s1 = {key:value for key, value in chars_in_s1.items()}
-                l_idx+=1
-                r_idx=l_idx
-        return False
-
-
-
