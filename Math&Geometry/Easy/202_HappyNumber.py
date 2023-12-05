604b9a8670bf084c68fea0e3b76ae90fc7289287
Nicholas Tanzillo <95975957+NicholasTanz@users.noreply.github.com>
2023-12-05 12:54:28 -0500

    Update 202_HappyNumber.py

--- Math&Geometry/Easy/202_HappyNumber.py
+++ Math&Geometry/Easy/202_HappyNumber.py
@@ -1,18 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def find_sum_squares(num):
-            curr_sum=0
-            num_as_str = str(num)
-            for char in num_as_str:
-                curr_sum+= int(char)**2
-            return curr_sum
-               
-        num_hash = {}
-        while True:
-            n = find_sum_squares(n)
-            if n == 1:
-                return True
-            if n in num_hash:
+        visitSet = set()
+
+        def squareDigits(num):
+            str_num = str(num)
+            square_sum = 0
+            for char in str_num:
+                square_sum += int(char)**2
+
+            return square_sum
+
+        while n != 1:
+            visitSet.add(n)
+            n = squareDigits(n)
+            if(n in visitSet):
                 return False
-            else:
-                num_hash[n] = True
+
+        return True
