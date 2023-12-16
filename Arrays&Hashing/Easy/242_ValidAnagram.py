be5c9282fad30f0c2e82b3285942dec88e8fa8a2
Nicholas Tanzillo <95975957+NicholasTanz@users.noreply.github.com>
2023-12-16 17:41:49 -0500

    Update 242_ValidAnagram.py

--- Arrays&Hashing/Easy/242_ValidAnagram.py
+++ Arrays&Hashing/Easy/242_ValidAnagram.py
@@ -1,19 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hash = defaultdict(int)
-        if len(s) != len(t):
-            return False
-             
-        for c in s:
-            hash[c] +=1
+        sLets = [0] * 26
+        for char in s:
+            sLets[ord(char) - ord('a')] += 1
         
-        for c in t:
-            if c in hash:
-                hash[c] -= 1
-                if hash[c] == 0:
-                    del hash[c]
-
-
-        if len(hash) == 0:
-            return True
-        return False
+        tLets = [0] * 26
+        for char in t:
+            tLets[ord(char) - ord('a')] += 1
+        
+        return tuple(tLets) == tuple(sLets)
